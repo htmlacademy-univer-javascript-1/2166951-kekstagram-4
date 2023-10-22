a4dba2b85621fce062c81e0a22e2ed694cca6c59
Keks <keks@htmlacademy.ru>
2023-10-22 18:34:21 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/utils.js
+++ 4/js/utils.js
@@ -16,3 +16,23 @@ export const getRandomInteger = (a, b) => {
 };
 
 export const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+export const checkStringLength = (input, maxLength) => input.length <= maxLength;
+
+export const isPalindrome = (input) => {
+  const string = input.toLowerCase().replaceAll(' ', '');
+  const reverseString = string.split('').reverse().join('');
+  return string === reverseString;
+};
+
+export const extractNumbers = (input) => {
+  const string = input.toString();
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    const char = string.at(i);
+    if (!isNaN(parseInt(char, 10))) {
+      result += char;
+    }
+  }
+  return result ? +result : NaN;
+};
